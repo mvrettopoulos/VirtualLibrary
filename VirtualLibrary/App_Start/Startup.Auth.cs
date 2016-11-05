e79a55557aa0bb20332269fe19f3d3831b338085
Michalis Vrettopoulos <michalis@DESKTOP-0KNCB6K>
2016-11-05 11:55:19 +0200

    fixed a bug on RoleManager, added navbar mappings

--- VirtualLibrary/App_Start/Startup.Auth.cs
+++ VirtualLibrary/App_Start/Startup.Auth.cs
@@ -17,6 +17,7 @@ namespace VirtualLibrary.App_Start
         {
             // Configure the db context, user manager and signin manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
