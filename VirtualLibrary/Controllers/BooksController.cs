b8147ba6698aca2db74bcee86b0f4ded57ac8337
Stathis Katsaros <stathis.katsaros@iteam.gr>
2016-11-08 22:22:22 +0200

    Add BookController and Views

---
+++ VirtualLibrary/Controllers/BooksController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VirtualLibrary.Models;
+
+namespace VirtualLibrary.Controllers
+{
+    public class BooksController : Controller
+    {
+        private VirtualLibraryEntities db = new VirtualLibraryEntities();
+
+        // GET: Books
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View(db.Books.ToList());
+        }
+
+        // GET: Books/Details/5
+        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Moderator")]
+        [Authorize(Roles = "User")]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Books books = db.Books.Find(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            return View(books);
+        }
+
+        // GET: Books/Create
+        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Moderator")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Moderator")]
+        public ActionResult Create([Bind(Include = "id,title,author_id,description,category_id,image,isbn,publisher")] Books books)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Books.Add(books);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(books);
+        }
+
+        // GET: Books/Edit/5
+        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Moderator")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Books books = db.Books.Find(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            return View(books);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Moderator")]
+        public ActionResult Edit([Bind(Include = "id,title,author_id,description,category_id,image,isbn,publisher")] Books books)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(books).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(books);
+        }
+
+        // GET: Books/Delete/5
+        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Moderator")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Books books = db.Books.Find(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            return View(books);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Moderator")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Books books = db.Books.Find(id);
+            db.Books.Remove(books);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
