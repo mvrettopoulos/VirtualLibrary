9860d232b8ceace1f22dbaccdd63c41ac99d2d15
Michalis Vrettopoulos <mvrettopoulos@gmail.com>
2016-11-09 17:41:29 +0200

    added more columns on User model

--- VirtualLibrary/Models/Users.cs
+++ VirtualLibrary/Models/Users.cs
@@ -31,6 +31,8 @@ namespace VirtualLibrary.Models
         public string aspnet_user_id { get; set; }
         public bool active { get; set; }
         public Nullable<bool> bad_user { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Books_Ratings> Books_Ratings { get; set; }
