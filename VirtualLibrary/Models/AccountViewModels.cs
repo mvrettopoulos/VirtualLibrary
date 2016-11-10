0d9cac71fef3ab49769789ca436b56a71b692a62
tatsistog <tatsistog1@hotmail.com>
2016-11-10 22:06:05 +0200

    Created the external login view. The results are being saved in database.

--- VirtualLibrary/Models/AccountViewModels.cs
+++ VirtualLibrary/Models/AccountViewModels.cs
@@ -6,7 +6,32 @@ namespace VirtualLibrary.Models
 {
     public class ExternalLoginConfirmationViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(15, MinimumLength = 3)]
+        [Display(Name = "User Name")]
+        [RegularExpression(@"(\S)+", ErrorMessage = " White Space is not allowed in User Names")]
+        [ScaffoldColumn(false)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "First Name is required.")]
+        [StringLength(20, MinimumLength = 3)]
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        [StringLength(15, MinimumLength = 3)]
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth is required")]
+        [MinimumAge(18)]
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public string Date_of_Birth { get; set; }
+
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
     }
