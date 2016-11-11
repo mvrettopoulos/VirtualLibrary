b9eba392da490b1d657b0cb18de258cdec17b562
Michalis Vrettopoulos <mvrettopoulos@gmail.com>
2016-11-11 20:49:25 +0200

    initial commit for search controller

--- VirtualLibrary/Controllers/SearchController.cs
+++ VirtualLibrary/Controllers/SearchController.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using VirtualLibrary.Models;
 
 namespace VirtualLibrary.Controllers
 {
     public class SearchController : Controller
     {
+        VirtualLibraryEntities db = new VirtualLibraryEntities();
         // GET: Search
         public ActionResult Index()
         {
-            return View();
+            return View(db.Books.OrderByDescending(s => s.views).ToList());
         }
 
         // GET: Search/Books
@@ -20,10 +23,20 @@ namespace VirtualLibrary.Controllers
             return View();
         }
 
-        // GET: Search/Book
-        public ActionResult GetBook()
+        // GET: Search/Book/Details/5
+        [AllowAnonymous]
+        public ActionResult GetBook(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Books books = db.Books.Find(id);
+            if (books == null)
+            {
+                return HttpNotFound();
+            }
+            return View(books);
         }
     }
 }
