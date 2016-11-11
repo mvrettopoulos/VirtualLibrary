ec397f888f70d35fda8b07988456ba2657f64e79
Michalis Vrettopoulos <mvrettopoulos@gmail.com>
2016-11-11 15:03:00 +0200

    added twiter authentication

--- VirtualLibrary/App_Start/Startup.cs
+++ VirtualLibrary/App_Start/Startup.cs
@@ -64,7 +64,15 @@ namespace VirtualLibrary.App_Start
             {
                 ConsumerKey = ConfigurationManager.AppSettings["TwiterClientID"],
                 ConsumerSecret = ConfigurationManager.AppSettings["TwiterClientSecret"],
-                
+                BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(new[]
+               {
+                    "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+                    "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+                    "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
+                    "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+                    "5168FF90AF0207753CCCD9656462A212B859723B", //DigiCert SHA2 High Assurance Server C‎A 
+                    "B13EC36903F8BF4701D498261A0802EF63642BC3" //DigiCert High Assurance EV Root CA
+                })
             });
             app.UseFacebookAuthentication(
                appId: ConfigurationManager.AppSettings["FacebookClientID"],
