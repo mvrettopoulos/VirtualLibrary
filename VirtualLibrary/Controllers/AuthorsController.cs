7d2fee3dfc82de64567d7249964f1d01dcfbe5fc
Michalis Vrettopoulos <mvrettopoulos@gmail.com>
2016-11-12 18:32:27 +0200

    Added Authors dashboard fully functional, and fixed bug with notifications

---
+++ VirtualLibrary/Controllers/AuthorsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VirtualLibrary.Models;
+namespace VirtualLibrary.Controllers
+{
+    [Authorize]
+    public class AuthorsController : Controller
+    {
+        private VirtualLibraryEntities db = new VirtualLibraryEntities();
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+   (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public ActionResult Index()
+        {
+            var authors = db.Author;
+            if (authors != null)
+            {
+                return View(authors.ToList());
+            }
+            else
+            {
+                return View("Authors");
+            }
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView("_Create");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AuthorViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Author author = new Author();
+                author.author_name = model.authorName;
+                db.Author.Add(author);
+                db.SaveChanges();
+                log.Info("Author created.");
+                return Json(new { success = true });
+            }
+            return PartialView("_Create", model);
+        }
+        // GET: Authors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Author author = db.Author.Find(id);
+            if (author == null)
+            {
+                return HttpNotFound();
+            }
+            return View(author);
+        }
+        // GET: Authors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Author author = db.Author.Find(id);
+
+            if (author == null)
+            {
+                return HttpNotFound();
+            }
+            var model = new AuthorViewModel();
+            model.authorName = author.author_name;
+            return PartialView("_Edit", model);
+        }
+        // POST: Authors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(AuthorViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Author author = new Author();
+                author.id = Convert.ToInt32(model.id);
+                author.author_name = model.authorName;
+                try
+                {
+                    db.Entry(author).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                catch (DataException e)
+                {
+                    log.Error("Database error:", e);
+                }
+                log.Info("Author updated.");
+                return Json(new { success = true });
+            }
+            return PartialView("_Edit", model);
+        }
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Author author = db.Author.Find(id);
+            if (author == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_Delete", @author);
+        }
+        // POST: Authors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var author = db.Author.Find(id);
+            db.Author.Remove(author);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ VirtualLibrary/Scripts/js/authors.view.js
@@ -0,0 +1,127 @@
+﻿var authorsView = {};
+authorsView.table = '';
+authorsView.table_row_data = '';
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip({
+        placement: 'bottom'
+    });
+    authorsView.table = $('#authors_overview_table').DataTable({
+        select: {
+            style: 'single'
+        },
+        columns: [
+            { "data": "id" },
+            { "data": "author_name" }
+        ]
+    });
+});
+$('#authors_overview_table tr').not(':first').hover(
+    function () {
+        $(this).css("background-color", "red");
+        $(this).css("color", "white");
+    },
+    function () {
+        $(this).css("background-color", "");
+        $(this).css("color", "");
+    }
+);
+$('#authors_overview_table').on('click', 'tr', function () {
+    authorsView.table_row_data = authorsView.table.row(this).data();
+});
+$('#insert_author_button').on('click', function (e) {
+    e.preventDefault();
+    $('#insert_author_modal').modal('show');
+    bindFormInsert();
+});
+function bindFormInsert(dialog) {
+    $('form', dialog).submit(function (e) {
+        e.preventDefault();
+        if (!$(this).valid()) {
+            return false;
+        }
+        $.ajax({
+            url: this.action,
+            type: this.method,
+            data: $(this).serialize()
+        }).done(function (result) {
+            if (result.success) {
+                $('#insert_author_modal').modal('hide');
+                location.reload();
+            } else {
+                $('#insert_author_modalContent').html(result);
+                bindFormInsert();
+            }
+        }).fail(function (xhr, status, error) {
+            alert('failed');
+        });
+    });
+}
+$('#delete_author_button').on('click', function () {
+    if (authorsView.table.rows('.selected').data().length == 0) {
+        $('#delete_author_button').prop('disabled', true);
+        $('#authors_view_notification').notify({
+            message: { text: 'No author has been selected' },
+            fadeOut: { enabled: true, delay: 2000 },
+            closable: false,
+            type: 'blackgloss',
+            onClose: function () {
+                $('#delete_author_button').prop('disabled', false);
+            }
+        }).show();
+    }
+    else {
+        var url = $('#delete_author_modal').data('url') + '/' + authorsView.table_row_data.id;
+        $.get(url, function (data) {
+            $('#delete_author_modal_body').html(data);
+            $('#delete_author_modal').modal('show');
+        });
+    }
+});
+$('#edit_author_button').on('click', function () {
+    if (authorsView.table.rows('.selected').data().length == 0) {
+        $('#edit_author_button').prop('disabled', true);
+        $('#authors_view_notification').notify({
+            message: { text: 'No author has been selected' },
+            fadeOut: { enabled: true, delay: 2000 },
+            closable: false,
+            type: 'blackgloss',
+            onClose: function () {
+                $('#edit_author_button').prop('disabled', false);
+            }
+        }).show();
+    }
+    else {
+        var url = $('#edit_author_modal').data('url') + '/' + authorsView.table_row_data.id;
+        $.get(url, function (data) {
+            $('#edit_author_modalContent').html(data);
+            $('#edit_author_modal').modal('show');
+            bindFormEdit();
+        });
+    }
+});
+function bindFormEdit(dialog) {
+    $('form', dialog).submit(function (e) {
+        e.preventDefault();
+        if (!$(this).valid()) {
+            return false;
+        }
+        $.ajax({
+            url: this.action,
+            type: this.method,
+            data: $(this).serialize()
+        }).done(function (result) {
+            if (result.success) {
+                $('#edit_author_modal').modal('hide');
+                location.reload();
+            } else {
+                $('#edit_author_modalContent').html(result);
+                bindFormEdit();
+            }
+        }).fail(function (xhr, status, error) {
+            alert('failed');
+        });
+    });
+}
+$('#delete_author_modal_submit_button').on('click', function () {
+    $('#delete_author_form').submit();
+});
