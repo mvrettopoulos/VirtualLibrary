7e7d460a2c04926f8267b21f40c53b5c2d71244a
Michalis Vrettopoulos <mvrettopoulos@gmail.com>
2016-11-14 16:27:50 +0200

    styling and added librarian dashboard

---
+++ VirtualLibrary/Controllers/LibrarianDashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VirtualLibrary.Controllers
+{
+    public class LibrarianDashboardController : Controller
+    {
+        // GET: LibrarianDashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
