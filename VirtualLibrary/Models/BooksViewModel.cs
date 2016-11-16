548a890a6c2427ca098c122113d65be6a822911d
Stathis Katsaros <stathis.katsaros@iteam.gr>
2016-11-17 01:16:39 +0200

    Fixed Authors and Categories MultiSelect but not working the others fields.

---
+++ VirtualLibrary/Models/BooksViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace VirtualLibrary.Models
+{
+    public class BooksViewModel
+    {
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(200, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Book Title")]
+        public string title { get; set; }
+
+        [Required]
+        [StringLength(int.MaxValue, ErrorMessage = "Please enter a descrption of the book.", MinimumLength = 50)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Description")]
+
+        public string description { get; set; }
+        [Display(Name = "Image")]
+        public byte[] image { get; set; }
+
+        [StringLength(60, ErrorMessage = "The ISBN should be at least 5 characters", MinimumLength = 5)]
+        [Display(Name = "ISBN")]
+        public string isbn { get; set; }
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)]
+        [Display(Name = "Publisher")]
+        public string publisher { get; set; }
+
+        public int views { get; set; }
+
+        public IEnumerable<SelectListItem> AllAuthors { get; set; }
+        public int[] ThisAuthor { get; set; }
+        public Books book { get; set; }
+
+        public IEnumerable<SelectListItem> AllCategories { get; set; }
+        public int[] ThisCategory { get; set; }
+       
+
+
+        public BooksViewModel()
+        {
+            AllAuthors = new List<SelectListItem>();
+            AllCategories = new List<SelectListItem>();
+        }
+
+    }
+}
