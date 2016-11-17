015ef02dcef306fa19241b57278f6269f3149ba2
DESKTOP-B3I1VNT\Ele <e.pavlatou@gmail.com>
2016-11-17 13:58:37 +0200

    profile info to edit profile den einai etoimo

---
+++ VirtualLibrary/Scripts/js/upload.view.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('#upload').click(function () {
+        var url = $('#insert_image_modal').data('url');
+
+        $.get(url, function (data) {
+           
+
+            $('#insert_image_modal').modal('show');
+        });
+    });
+});
