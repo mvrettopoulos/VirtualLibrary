a0e17c259e0c05d2bd8d2b5977ef7b9c372a853a
Michalis Vrettopoulos <mvrettopoulos@gmail.com>
2016-11-23 16:21:11 +0200

    giving more functionality to datepicker

--- VirtualLibrary/Scripts/js/reserve.book.view.js
+++ VirtualLibrary/Scripts/js/reserve.book.view.js
@@ -1,4 +1,4 @@
-﻿var startDate, endDate, dateRange = [];
+﻿var startDate, endDate, ranges = [];
 
 $("#librarySelect").change(function(){
     var thisvalue = $(this).find("option:selected").val();
@@ -7,12 +7,27 @@ $("#librarySelect").change(function(){
         if (reservationDatesLibraries[i].library == thisvalue) {
             var dateRangesList = reservationDatesLibraries[i].datesRange;
             for (var k = 0; k < dateRangesList.length; k++) {
-                endDate = jQuery.datepicker.formatDate('dd-mm-yy', dateRangesList[k].endDate);
-                for (var d = new Date(jQuery.datepicker.formatDate('dd-mm-yy', dateRangesList[k].startDate)) ;
-                    d <= new Date(jQuery.datepicker.formatDate('dd-mm-yy', dateRangesList[k].endDate)) ;
-                    d.setDate(d.getDate() + 1)) {
-                    dateRange.push($.datepicker.formatDate('yy-mm-dd', d));
-                }
+
+
+                var startDateArray = dateRangesList[k].startDate.split('-');
+                // rearrange the month and day
+                startDateArray.splice(1, 0, startDateArray.shift());
+                // create a date object called date and pass our array to the constructor
+                var startDate = new Date(startDateArray);
+
+                var endDateArray = dateRangesList[k].endDate.split('-');
+                // rearrange the month and day
+                endDateArray.splice(1, 0, endDateArray.shift());
+                // create a date object called date and pass our array to the constructor
+                var endDate = new Date(endDateArray);
+                ranges.push({ start: startDate, end: endDate });
+
+                //endDate = jQuery.datepicker.formatDate('dd-mm-yy', dateRangesList[k].endDate);
+                //for (var d = new Date(jQuery.datepicker.formatDate('dd-mm-yy', dateRangesList[k].startDate)) ;
+                //    d <= new Date(jQuery.datepicker.formatDate('dd-mm-yy', dateRangesList[k].endDate)) ;
+                //    d.setDate(d.getDate() + 1)) {
+                //    
+                //}
             }
             setReserveDatepicker();
             setReturnDatepicker();
@@ -35,11 +50,26 @@ function setReserveDatepicker(){
             $('#datepickerReturn').datepicker('setDate', date2);
             //sets minDate to dt1 date + 1
             $('#datepickerReturn').datepicker('option', 'minDate', date2);
-            $('#datepickerReturn').datepicker('option', 'maxDate', date2.getDate() - 11);
+
+            var tmpDate = Date.parse(date2);
+            console.log("Datepicker :"+tmpDate);
+            for (var i = 0; i < ranges.length; i++) {
+                var startDate = Date.parse(ranges[i].start);
+               // var endDate = Date.parse(ranges[i].end);
+                if (tmpDate >= startDate) {
+                    
+                    $('#datepickerReturn').datepicker('option', 'maxDate', ranges[i].end);
+                    
+                    
+                }
+            }
+           
         },
         beforeShowDay: function (date) {
-            var dateString = jQuery.datepicker.formatDate('yy-mm-dd', date);
-            return [dateRange.indexOf(dateString) == -1];
+            for (var i = 0; i < ranges.length; i++) {
+                if (date >= ranges[i].start && date <= ranges[i].end) return [false, ''];
+            }
+            return [true, ''];
         }
     });
 }
@@ -50,16 +80,12 @@ function setReturnDatepicker() {
         dateFormat: "dd-mm-yy",
         onClose: function () {
             var dt1 = $('#datepickerReserve').datepicker('getDate');
-            console.log(dt1);
+            
             var dt2 = $('#datepickerReturn').datepicker('getDate');
             if (dt2 <= dt1) {
                 var minDate = $('#datepickerReturn').datepicker('option', 'minDate');
                 $('#datepickerReturn').datepicker('setDate', minDate);
             }
-        },
-        beforeShowDay: function (date) {
-            var dateString = jQuery.datepicker.formatDate('yy-mm-dd', date);
-            return [dateRange.indexOf(dateString) == -1];
         }
     });
 }
