654ae44b7c35d7506a2c5f16688c90f08ed031f6
Michas Nikolaos <nikos.mixas@outlook.com>
2016-12-03 12:28:03 +0200

    simplier password security

--- VirtualLibrary/App_Start/IdentityConfig.cs
+++ VirtualLibrary/App_Start/IdentityConfig.cs
@@ -49,9 +49,9 @@ namespace VirtualLibrary.App_Start
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false
             };
             // Configure user lockout defaults
             manager.UserLockoutEnabledByDefault = true;
